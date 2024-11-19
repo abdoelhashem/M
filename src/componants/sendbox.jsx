fe7c7619caa562772b2e527f16ba343b86876449
abdo ahmed <146590912+abdoelhashem@users.noreply.github.com>
2024-11-19 04:04:30 +0200

    Update sendbox.jsx

--- src/componants/sendbox.jsx
+++ src/componants/sendbox.jsx
@@ -4,10 +4,16 @@ const Sendbox = ({publicUrl}) => {
     const [invalue,setInvalue] = useState("");
     const [ine,setIne] = useState(true);
     const [ip,setIp] = useState("");
+    const [vist,setVist] = useState("");
     useEffect(() => {
         fetch('https://api.ipify.org?format=json')
         .then(response => response.json())
         .then(data => setIp(data.ip))
+        .catch(error => console.error("حدث خطأ:", error));
+
+        fetch('https://abdoelhashem.pythonanywhere.com/views')
+        .then(response => response.json())
+        .then(data => setVist(data.view))
         .catch(error => console.error("حدث خطأ:", error));
     }, [])
     const v = (x) => {
@@ -35,6 +41,7 @@ const Sendbox = ({publicUrl}) => {
     
             <textarea onInput={v} value={invalue} className="input-box" placeholder="اكتب رسالتك هنا..." rows="4"></textarea>
             <button className="send-btn" onClick={send}>{ine ? 'send' : <div className="flex justify-center"><div class="loader vv"></div></div>}</button>
+            <div className="message-footer pt-3">الزيارات : {vist}</div>
         </div>
 
     )
